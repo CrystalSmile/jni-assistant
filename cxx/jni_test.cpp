35055ca1d65b5b509e40bcc644ca1fe34330075e
周晓 <zhou7469@gmail.com>
2020-06-23 13:15:41 +0800

    no message

--- cxx/jni_test.cpp
+++ cxx/jni_test.cpp
@@ -9,13 +9,9 @@ JNIEXPORT void JNICALL Java_com_smile_crystal_JNITest_testPrint
 	printf("from cxx Hello! \n");
 }
 
-/*
- * Class:     com_smile_crystal_JNITest
- * Method:    testAdd
- * Signature: (II)I
- */
 JNIEXPORT jint JNICALL Java_com_smile_crystal_JNITest_testAdd
 (JNIEnv*, jobject, jint a, jint b)
 {
 	return a + b;
 }
+
